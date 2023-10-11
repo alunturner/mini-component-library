ba4bb823fe6d746c7b3315ff819b65c652417dc6
alunturner <alun.turner@googlemail.com>
2023-10-11 18:10:33 +0100

    finish progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,20 +8,38 @@ import VisuallyHidden from '../VisuallyHidden';
 const SIZES = {
     small: {
         "--height": "8px",
+        "--borderRadius": "4px",
     },
     medium: {
         "--height": "12px",
+        "--borderRadius": "4px",
     },
     large: {
         "--height": "24px",
+        "--borderRadius": "8px",
+        "--padding": "4px",
     }
 }
 const ProgressBar = ({ value, size }) => {
-    return <Progress style={SIZES[size]}role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow={value}>{value}</Progress>;
+    return <ProgressWrapper style={SIZES[size]}role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow={value}><ProgressContent value={value}/></ProgressWrapper>;
 };
 
-const Progress = styled.div`
+const ProgressWrapper = styled.div`
+    border-radius: var(--borderRadius);
+    background: ${COLORS.transparentGray15};
+    box-shadow: 0px 2px 4px 0px rgba(128, 128, 128, 0.35) inset;
     width: 370px;
     height: var(--height);
+    padding: var(--padding);
+`
+
+const ProgressContent = styled.div`
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    border-top-right-radius: ${p => p.value === 100 && "4px"};
+    border-bottom-right-radius: ${p => p.value === 100 && "4px"};
+    background-color: ${COLORS.primary};
+    width: ${p => `${p.value}%`};
+    height: 100%;
 `
 export default ProgressBar;
