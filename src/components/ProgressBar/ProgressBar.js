58e648bec2d8b1fa0cf5513c53ac3a40e957d92f
alunturner <alun.turner@googlemail.com>
2023-10-11 17:50:40 +0100

    add sizing

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,11 +5,23 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+    small: {
+        "--height": "8px",
+    },
+    medium: {
+        "--height": "12px",
+    },
+    large: {
+        "--height": "24px",
+    }
+}
 const ProgressBar = ({ value, size }) => {
-    return <Progress role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow={value}>{value}</Progress>;
+    return <Progress style={SIZES[size]}role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow={value}>{value}</Progress>;
 };
 
 const Progress = styled.div`
-    height: auto;
+    width: 370px;
+    height: var(--height);
 `
 export default ProgressBar;
