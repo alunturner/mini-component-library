1e9cd551cce08c757c7bf2fcbf127356345ca881
alunturner <alun.turner@googlemail.com>
2023-10-13 13:54:03 +0100

    add focus handling

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -29,6 +29,14 @@ const Container = styled.div`
     background: ${COLORS.transparentGray15};
 `
 
+// could have set top and left to 0, then set width and height to 100%
+    const NativeSelect = styled.select`
+position: absolute;
+width: 100%;
+padding: 12px 16px;
+opacity: 0%;
+`
+
 // alternative to flex box would have been absolute icon positioning
 const Wrapper = styled.div`
     width: auto;
@@ -40,18 +48,14 @@ const Wrapper = styled.div`
     font-style: normal;
     font-weight: 400;
     line-height: normal;
+    
+    ${NativeSelect}:focus + & {
+        border: 2px solid blue;
+    }
 `
 
 const Span = styled.span`
     margin-right: 24px;
-`
-
-// could have set top and left to 0, then set width and height to 100%
-const NativeSelect = styled.select`
-    position: absolute;
-    width: 100%;
-    padding: 12px 16px;
-    opacity: 0%;
 `
 
 export default Select;
